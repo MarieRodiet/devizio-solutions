a0ea40da5b87bcf0b902f7d6c09b16844c733f66
MarieRodiet <marie.rodiet@gmail.com>
2025-03-03 11:44:50 +0100

    build app folder structure

--- devizio-solutions/src/App.jsx
+++ devizio-solutions/src/App.jsx
@@ -1,7 +1,12 @@
 import './App.css';
+import Home from './containers/Home/Home';
 
 function App() {
-	return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+	return (
+		<main>
+			<Home></Home>
+		</main>
+	);
 }
 
 export default App;
