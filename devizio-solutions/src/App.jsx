fcbf99719fbc64d83d344edba7c8dcc6af003706
MarieRodiet <marie.rodiet@gmail.com>
2025-03-04 16:28:16 +0100

    add router to navigate to pages

--- devizio-solutions/src/App.jsx
+++ devizio-solutions/src/App.jsx
@@ -1,10 +1,35 @@
 import './App.css';
 import Home from './containers/Home/Home';
+import Header from './components/Header/Header';
+import About from './containers/About/About';
+import Login from './containers/Login/Login';
+import Register from './containers/Register/Register';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
 	return (
 		<main>
-			<Home></Home>
+			<Header />
+			<Router>
+				<Routes>
+					<Route
+						path="/"
+						element={<Home />}
+					/>
+					<Route
+						path="/about"
+						element={<About />}
+					/>
+					<Route
+						path="/login"
+						element={<Login />}
+					/>
+					<Route
+						path="/register"
+						element={<Register />}
+					/>
+				</Routes>
+			</Router>
 		</main>
 	);
 }
